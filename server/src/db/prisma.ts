8a567fc521afa014f267d1d90f709e353d9415ca
cdaugherty <cdaugherty@fullsail.edu>
2026-08-19 14:40:38 -0400

    prisma

---
+++ server/src/db/prisma.ts
@@ -0,0 +1,27 @@
+import "dotenv/config";
+import { PrismaClient } from "../generated/prisma/client.ts";
+import { PrismaPg } from "@prisma/adapter-pg";
+
+const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
+const prisma = new PrismaClient({ adapter });
+
+const connectDB = async () => {
+  try {
+    await prisma.$connect();
+    console.log("Connected to the database");
+  } catch (error) {
+    console.error("Error connecting to the database:", error);
+    process.exit(1);
+  }
+};
+
+const disconnectDB = async () => {
+  try {
+    await prisma.$disconnect();
+    console.log("Disconnected from the database");
+  } catch (error) {
+    console.error("Error disconnecting from the database:", error);
+  }
+};
+
+export { prisma, connectDB, disconnectDB };
