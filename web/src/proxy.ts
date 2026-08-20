531093ecde7102fa9ec8044c8bacbe3bcb5a2782
cdaugherty <cdaugherty@fullsail.edu>
2026-08-20 12:18:51 -0400

    protected routes

---
+++ web/src/proxy.ts
@@ -0,0 +1,34 @@
+import { NextRequest, NextResponse } from "next/server";
+
+// 1. Specify protected and public routes
+const protectedRoutes = ["/dashboard"];
+const publicRoutes = ["/login", "/signup", "/"];
+
+export default async function proxy(req: NextRequest) {
+  // 2. Check if the current route is protected or public
+  const path = req.nextUrl.pathname;
+  const isProtectedRoute = protectedRoutes.some(
+    (route) => path === route || path.startsWith(`${route}/`),
+  );
+  const isPublicRoute = publicRoutes.includes(path);
+
+  // 3. Get the JWT from the cookie
+  const jwt = req.cookies.get("jwt")?.value;
+
+  // 4. Redirect to /login if the user is not authenticated
+  if (isProtectedRoute && !jwt) {
+    return NextResponse.redirect(new URL("/login", req.nextUrl));
+  }
+
+  // 5. Redirect to /dashboard if the user is authenticated
+  if (isPublicRoute && jwt) {
+    return NextResponse.redirect(new URL("/dashboard", req.nextUrl));
+  }
+
+  return NextResponse.next();
+}
+
+// Routes Proxy should not run on
+export const config = {
+  matcher: ["/((?!api|_next/static|_next/image|.*\\.png$).*)"],
+};
